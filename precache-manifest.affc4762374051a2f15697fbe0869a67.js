c6f482019ce032c26a2940289fd2ffa109c67bdf
Cyo Roolynn <reallife_data@hotmail.com>
2019-04-12 14:32:15 +1000

    React Initial Build

---
+++ precache-manifest.affc4762374051a2f15697fbe0869a67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bfe9af6b7bb780e49056",
+    "url": "/static/css/main.b5a0d189.chunk.css"
+  },
+  {
+    "revision": "bfe9af6b7bb780e49056",
+    "url": "/static/js/main.a359cae6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6b1d956d7f4c9153417bcd8a48348b73",
+    "url": "/index.html"
+  }
+];
